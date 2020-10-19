b9b3e5c1a08c74168f170f1f519955c24b93d4b4
donna <donnafochler@gmail.com>
2020-10-19 00:50:59 -0400

    campsiteInfo component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
 import { CAMPSITES } from './shared/campsites';
+import CampsiteInfo from './components/CampsiteInfoComponent';
 
 class App extends Component {
   constructor(props) {
