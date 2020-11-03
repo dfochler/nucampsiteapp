3fe5ffc7f8368d0acce9d29531f7b30af65a59b8
donna <donnafochler@gmail.com>
2020-11-03 00:26:50 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,7 +23,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <>
@@ -47,7 +47,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
           );
         })}
       
-      <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+      <CommentForm campsiteId={campsiteId} postComment={postComment}/>
       </div>
       </>
     );
@@ -94,7 +94,7 @@ export default function CampsiteInfo(props) {
           <div className="row">
             <RenderCampsite campsite={props.campsite} />
             <RenderComments comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             campsiteId={props.campsite.id}/>
           </div>
         </div>
@@ -128,7 +128,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+      this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
